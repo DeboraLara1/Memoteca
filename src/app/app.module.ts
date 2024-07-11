29260044c6bb661b7b13380a499d0e6858f38065
Debora <debora.roldanlara@gmail.com>
2024-07-11 16:53:56 -0300

    Configurações da estrutura inicial do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CabecalhoComponent
   ],
   imports: [
     BrowserModule,
